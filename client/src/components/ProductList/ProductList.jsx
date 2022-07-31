d4a4a5bd5c7889818ab7d23cd8b5b63fecd8b329
Deemoras <deemoras@publicisgroupe.net>
2022-07-31 15:54:25 +0530

    fixed product page css

--- client/src/components/ProductList/ProductList.jsx
+++ client/src/components/ProductList/ProductList.jsx
@@ -1,43 +1,64 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import axios from 'axios';
-import './ProductListStyle.scss';
-import Button from '@mui/material/Button';
+import axios from "axios";
+import "./ProductListStyle.scss";
+import Button from "@mui/material/Button";
 
 export default function ProductList(props) {
-
-    
   return (
-    <div className='prod-container'>
-        {props.productsList && props.productsList.length && props.productsList.map((item,i)=>{
-            return (
-                <div key={item.id} className="card-style">
-                    <div className='card-title'>
-                        <h3>{item.name}</h3>
-                    </div>
-                    <div className='mobile-display-card'>
-                        <div className='card-img'>
-                            <img
-                            src={item.imageURL}
-                            alt="logo"
-                            height={150}
-                            width={200}
-                            />
-                        </div>
-                        <div>
-                            <p className='card-desc-style'>
-                                {item.description}
-                            </p>
-                            <div className='price-row'>
-                                MRP Rs.{item.price}
-                                <Button variant="contained" className='prod-btn-style' onClick={() => props.addItemsToCart(item)}>Buy Now</Button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='border-style'></div>
-                </div>
-            )
+    <div className="prod-container">
+      {props.productsList &&
+        props.productsList.length &&
+        props.productsList.map((item, i) => {
+          return (
+            <div key={item.id} className="item-card-container">
+              <div className="card-title">
+                <p>{item.name}</p>
+              </div>
+              <div className="card-img">
+                <img src={item.imageURL} />
+              </div>
+              <div className="card-desc">
+                <p>{item.description}</p>
+              </div>
+              <div className="card-footer">
+                MRP Rs.{item.price}
+                <Button
+                  variant="contained"
+                  className="prod-btn-style"
+                  onClick={() => props.addItemsToCart(item)}
+                >
+                  Buy Now
+                </Button>
+              </div>
+            </div>
+            // <div key={item.id} className="card-style">
+            //     <div className='card-title'>
+            //         <h3>{item.name}</h3>
+            //     </div>
+            //     <div className='mobile-display-card'>
+            //         <div className='card-img'>
+            //             <img
+            //             src={item.imageURL}
+            //             alt="logo"
+            //             height={150}
+            //             width={200}
+            //             />
+            //         </div>
+            //         <div>
+            //             <p className='card-desc-style'>
+            //                 {item.description}
+            //             </p>
+            //             <div className='price-row'>
+            //                 MRP Rs.{item.price}
+            //                 <Button variant="contained" className='prod-btn-style' onClick={() => props.addItemsToCart(item)}>Buy Now</Button>
+            //             </div>
+            //         </div>
+            //     </div>
+            //     <div className='border-style'></div>
+            // </div>
+          );
         })}
     </div>
-  )
+  );
 }
